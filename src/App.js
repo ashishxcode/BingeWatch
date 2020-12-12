a3b5f87e55bc2762487449659b180686ae1441b4
Ashish Patel <34959866+ashishxcode@users.noreply.github.com>
2020-12-12 14:53:05 +0530

    Search by genre added

--- src/App.js
+++ src/App.js
@@ -9,10 +9,35 @@ export default function App() {
     "https://api.themoviedb.org/3/search/movie?api_key=968a6cce98ad094cf7b4d6ceb3e74f7c&language=en-US&query=";
   let FEATURED_API =
     "https://api.themoviedb.org/3/discover/movie?api_key=968a6cce98ad094cf7b4d6ceb3e74f7c&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=2";
- 
-    const [movies, setMovie] = useState([]);
+  let GENRE_API =
+    "https://api.themoviedb.org/3/discover/movie?api_key=968a6cce98ad094cf7b4d6ceb3e74f7c&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&with_genres";
+  const genreList = {
+    28: "Action",
+    12: "Adventure",
+    16: "Animation",
+    35: "Comedy",
+    80: "Crime",
+    99: "Documentary",
+    18: "Drama",
+    10751: "Family",
+    14: "Fantasy",
+    36: "History",
+    27: "Horror",
+    10402: "Music",
+    9648: "Mystery",
+    10749: "Romance",
+    878: "Science Fiction",
+    10770: "TV Movie",
+    53: "Thriller",
+    10752: "War",
+    37: "Western"
+  };
+
+  const genreNameList = Object.values(genreList);
+
+  const [movies, setMovie] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
- 
+
   useEffect(() => {
     getMovies(FEATURED_API);
   }, []);
@@ -21,10 +46,13 @@ export default function App() {
     fetch(API)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
         setMovie(data.results);
       });
   };
+
+  function genreClickHandler(item) {
+    console.log(item);
+  }
 
   const onSubmitHandler = (e) => {
     e.preventDefault();
@@ -35,6 +63,22 @@ export default function App() {
       setSearchTerm("");
     }
   };
+
+  function getGenreKey(object, item) {
+    for (var prop in object) {
+      if (object.hasOwnProperty(prop)) {
+        if (object[prop] === item) {
+          return prop;
+        }
+      }
+    }
+  }
+
+  function genreClickHandler(item) {
+    let KEY = getGenreKey(genreList, item);
+    let genreQuery = `${GENRE_API}=${KEY}`;
+    getMovies(genreQuery);
+  }
 
   const onChangeHandler = (e) => {
     setSearchTerm(e.target.value);
@@ -56,6 +100,19 @@ export default function App() {
           />
         </form>
       </header>
+      <div className="genres-container">
+        {genreNameList.map((item) => {
+          return (
+            <span
+              className="genre-span"
+              key={item}
+              onClick={() => genreClickHandler(item)}
+            >
+              {item}
+            </span>
+          );
+        })}
+      </div>
       <div className="movie-container">
         {movies.map((item) => (
           <MovieComponent key={item.id} {...item} />
--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const IMAGE_API = "https://image.tmdb.org/t/p/w500/";
 
@@ -11,7 +11,45 @@ const setVoteClas = (vote) => {
     return "bad";
   }
 };
-const MovieComponent = ({ title, overview, poster_path, vote_average }) => (
+
+const genreList = {
+  28: "Action",
+  12: "Adventure",
+  16: "Animation",
+  35: "Comedy",
+  80: "Crime",
+  99: "Documentary",
+  18: "Drama",
+  10751: "Family",
+  14: "Fantasy",
+  36: "History",
+  27: "Horror",
+  10402: "Music",
+  9648: "Mystery",
+  10749: "Romance",
+  878: "Science Fiction",
+  10770: "TV Movie",
+  53: "Thriller",
+  10752: "War",
+  37: "Western"
+};
+let genreName = "Unknown";
+const getGenreName = (genre_ids) => {
+  const id = genre_ids[0];
+  const name = genreList[id];
+  if (name === undefined) {
+    genreName = "Unknown";
+  } else {
+    genreName = name;
+  }
+};
+const MovieComponent = ({
+  title,
+  overview,
+  poster_path,
+  vote_average,
+  genre_ids
+}) => (
   <div className="movie">
     <img
       src={
@@ -21,8 +59,13 @@ const MovieComponent = ({ title, overview, poster_path, vote_average }) => (
       }
       alt={title}
     />
-    <div className="movie-info">
+    <div className="movie-title">
       <p>{title}</p>
+    </div>
+    <div className="movie-info">
+      <span className="movie-genre">
+        {(getGenreName(genre_ids), genreName)}
+      </span>
       <span className={`tag ${setVoteClas(vote_average)}`}>{vote_average}</span>
     </div>
     <div className="movie-overview">
